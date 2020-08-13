9a4cada982bb32d18f85206de1269c6662629b3c
Aa Pu <aapu07@gmail.com>
2020-08-13 13:57:51 -0400

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Welcome from "./components/welcome/Welcome.jsx";
 import Clock from "./components/clock/Clock.jsx";
 import Contact from './components/contact/Contact';
+import Navigation from './components/navigation/Navigation';
+import NotFound from './components/notfound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <Welcome name="ISBC" />
-      <Clock />
-      <Contact />
+      <Navigation />
+
+      <Switch>
+
+        <Route
+          path="/welcome/:name"
+          render={(props) => { return <Welcome {...props} name={props.match.params.name} /> }}
+        />
+        <Route
+          exact
+          path="/"
+          render={(props) => <Welcome {...props} name="Davey Strus" />}
+        />
+        <Route path="/clock" component={Clock} />
+        <Route path="/contact" component={Contact} />
+        <Route path="*" component={NotFound} />
+
+      </Switch>
+
+
 
     </div>
   );
-}
 
+}
 export default App;
---
+++ src/components/notfound/NotFound.jsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+
+function NotFound(prop){
+
+    return (
+        <div> 
+            <h3> 404 NOT FOUND !!!! </h3>
+        </div>
+    )
+    
+   
+};
+
+export default NotFound;
